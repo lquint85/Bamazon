6a894a5b69cfb361f5d8bd10f1fc583e2c7d1595
Lucent <lquint85@gmail.com>
2018-12-13 00:07:20 -0600

    modified BamazonCustomer.js File

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,6 +1,9 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+require("console.table");
+// require("dotenv");
 
+//define connection
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -11,89 +14,86 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
+    // will connect to new Host
     password: "password",
     database: "bamazon_db"
 });
-
+//call and actually connect to database
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    promptForAction();
-    connection.end();
+    // once connection is successful then call getProducts function
+    getProducts();
+
 });
 
-
-function createItem() {
+// now query connection to get a list of all products stored in db
+function getProducts() {
     // console.log("Inserting a new product...\n");
-    var query = connection.query(
-        "INSERT INTO items SET ?", [{
-            name: "Mountain Dew",
-            category: "Beverage",
-            currentBid: 1
-        }],
-        function (err, res) {
-            // console.log(res.affectedRows + " item inserted!\n");
-            // Call updateProduct AFTER the INSERT completes
-            promptForAction();
+    connection.query("SELECT * FROM items ", function (err, res) {
+            if (err) throw err;
+            //if response code is 200 then response is consoled
+            console.table(res);
+            // calling the prompForAtion function
+            promptForAction(res);
 
         }
 
     );
 }
 
+//creates a list of questions from query
+function promptForAction(products) {
 
-function promptForAction() {
 
-
-    // Created a series of questions
+    // Created a series of questions 
     inquirer.prompt([
 
-        {
-            type: "list",
-            name: "action",
-            message: "What item would you like to purchase?",
-            choices: ["Food", "Clothes", "Shoes"]
-        }
-    ]);
-    
-    inquirer.prompt([
+            {
+                type: "input",
+                name: "user_Choice",
+                message: "What item id would you like?",
 
-        {
-            type: "list",
-            name: "action",
-            message: "How may unites would you like to purchase?",
-            choices: ["1", "2","3"]
-        }
-
-
+            },
+            {
+                type: "input",
+                name: "user_Quantity",
+                message: "How many products would you like to buy?",
+            },
+            //once user has responded to ? the user response is passed into the .then method
+        ])
+        .then(function (response) {
+            console.log(response.user_Choice, response.user_Quantity);
+            checkUserResponse(products, response);
 
 
-    ]).then(function (response) {
-        console.log(response.action);
-
-        if (response.action === "Post") {
-            console.log("Post");
-            createItem();
-        } else {
-            console.log("Bid");
-
-
-        }
-    });
+        })
 }
 
+function checkUserResponse(products, userResponse) {
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].id == userResponse.user_Choice) {
+            if (products[i].stock_quantity >= userResponse.user_Quantity) {
+                // console.log("transaction complete");
+                var updatedStock = products[i].stock_quantity - userResponse.user_Quantity;
+                //updating stock quantity decrease the stock from 100 to 99
+               /* create function that takes 2 parameters (1 product id userRespnse.userChoice 2 update stock ) use those two variables to update the mysql database
+               update or INSERT INTO */
 
-// function login() {
-//     inquirer.prompt([{
 
-//             message: "User Name:",
-//             name: "password"
-//         },  
-//         {
-//             message: "Password:",
-//             name: "password",
-//             type: "password"
-//         } 
-//     ])
-//     // connection.end();
-// };
+                return
+            } else {
+                console.log("insufficient quantity");
+                return
+            }
+        }
+        // else {
+
+        // }
+
+    }
+    console.log("product not found");
+
+    
+}
+// process.exit();
