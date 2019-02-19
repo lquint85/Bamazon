b4ffd2c1371b837671c5bb0727838b8abb18c145
Lucent <lquint85@gmail.com>
2019-02-18 21:22:54 -0600

    food order customer finished

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -6,94 +6,203 @@ var cTable = require("console.table");
 //define connection
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "Luis2018",
-
-    // Your password
-    // will connect to new Host
     password: "Luis2018",
     database: "bamazon_db"
 });
-//call and actually connect to database
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    // once connection is successful then call getProducts function
-    getProducts();
-
+    startPurchase();
 });
 
-// now query connection to get a list of all products stored in db
-function getProducts() {
-    // console.log("Inserting a new product...\n");
-    connection.query("SELECT * FROM items ", function (err, res) {
-            if (err) throw err;
-            //if response code is 200 then response is consoled
-            console.table(res);
-            // calling the prompForAtion function
-            promptForAction(res);
-
-        }
-
-    );
-}
-
-//creates a list of questions from query
-function promptForAction(products) {
-
-
-    // Created a series of questions 
-    inquirer.prompt([
-
-            {
-                type: "input",
-                name: "user_Choice",
-                message: "What item id would you like?",
-
-            },
-            {
-                type: "input",
-                name: "user_Quantity",
-                message: "How many products would you like to buy?",
-            },
-            //once user has responded to ? the user response is passed into the .then method
-        ])
-        .then(function (response) {
-            console.log(response.user_Choice, response.user_Quantity);
-            checkUserResponse(products, response);
-
-
-        })
-}
-
-function checkUserResponse(products, userResponse) {
-    for (var i = 0; i < products.length; i++) {
-        if (products[i].id == userResponse.user_Choice) {
-            if (products[i].stock_quantity >= userResponse.user_Quantity) {
-                // console.log("transaction complete");
-                var updatedStock = products[i].stock_quantity - userResponse.user_Quantity;
-                //updating stock quantity decrease the stock from 100 to 99
-               /* create function that takes 2 parameters (1 product id userRespnse.userChoice 2 update stock ) use those two variables to update the mysql database
-               update or INSERT INTO */
-
-
-                return
+//* ///////////////////////////////////////////////////////////////////////////
+//* start Purchase
+//* ///////////////////////////////////////////////////////////////////////////
+function startPurchase(answer) {
+    inquirer
+        .prompt([{
+            name: "myStore",
+            type: "confirm",
+            message: "Welcome to My Taqueria. Would you like to order something?",
+            default: false
+        }])
+        .then(function (answer) {
+            // console.log(answer);
+            if (answer.myStore === true) {
+                findItem();
             } else {
-                console.log("insufficient quantity");
-                return
+                console.log("----------------------------------------------------------");
+                console.log("---------------------See You Next Time--------------------");
+                console.log("----------------------------------------------------------");
+                connection.end();
             }
-        }
-        // else {
-
-        // }
-
-    }
-    console.log("product not found");
-
-    
+        });
 }
+
+//* ///////////////////////////////////////////////////////////////////////////
+//* Ordering
+//* ///////////////////////////////////////////////////////////////////////////
+
+function findItem() {
+    // console.log("Connected");
+    connection.query("SELECT * FROM bamazon_db.items", function (err, results) {
+        // console.log(results);
+        console.log('\n');
+        console.log("----------------------------------------------------------");
+        console.log("---------------------YUMMY IN MY TUMMY--------------------");
+        console.log("----------------------------------------------------------");
+        console.log('\n');
+        console.table(results);
+        if (err) throw err;
+        inquirer.prompt([{
+                    name: "product",
+                    type: "input",
+                    message: "What is the ID of the item you would like to order",
+                    validate: function (value) {
+                      
+                        if (
+                            isNaN(value) === false &&
+                            value <= results.length &&
+                            value != 0
+                        ) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                },
+                {
+                    name: "stock",
+                    type: "input",
+                    message: "How many would you like?",
+                    validate: function (value) {
+
+                        if (isNaN(value) === false && value != 0) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            ])
+            
+            .then(function (answer) {
+                // console.log(answer.productID);
+                // console.log(answer.stock);
+                var productID = parseInt(answer.product);
+
+                // console.log(productID);
+               
+                var productName = results.productName;
+                connection.query("SELECT * FROM bamazon_db.items WHERE id=" + productID,function (err, results) {
+                    console.log("----------------------------------------------------------");
+                        
+                    if (err) throw err;
+                        var product = results[0].product_name;
+                        var productCost = results[0].price;
+                        var quantity = (results[0].stock_quantity);
+                        let requestedQuantity = (answer.stock);
+                        //* ///////////////////////////////////////////////////////////////////////////
+                        //* Confirm Order
+                        //* ///////////////////////////////////////////////////////////////////////////
+                        inquirer.prompt([{
+                                name: "confirmation",
+                                type: "confirm",
+                                message: "You would like to purchase " +
+                                    answer.stock +
+                                    " " +
+                                    product +
+                                    " is this correct?",
+                                default: false
+                            }])
+                            .then(function (answer) {
+                                if (answer.confirmation === true) {
+                                    // console.log(requestedQuantity);
+                                    // console.log(quantity);
+                                    if (requestedQuantity <= quantity) {
+                                        // checkout();
+                                        // console.log("quantity1: " + quantity);
+                                        // console.log("answer_stock: (now quantity) ", (requestedQuantity + 1));
+                                        let newQuantity = quantity - requestedQuantity;
+                                        // console.log("----------------------------------------------------------");
+                                        // console.log("" + " REMAINING QUANTITY: " + newQuantity);
+                                        console.log("----------------------------------------------------------");
+                                        // console.log("requestedQuantity" + requestedQuantity)
+                                        // console.log(newQuantity);
+                                        let totalCost = (requestedQuantity * productCost).toFixed(2);
+
+
+                                        console.log('\n');
+                                        console.log("----------------------------------------------------------");
+                                        console.log("Total Cost:" +  " "  + requestedQuantity + " " + product + " = $ " + totalCost);
+                                        console.log("----------------------------------------------------------");
+                                        console.log('\n');
+
+           
+                                        updateInventory();
+                                        //* ///////////////////////////////////////////////////////////////////////////
+                                        //* Update Inventory 
+                                        //* ///////////////////////////////////////////////////////////////////////////
+                                        function updateInventory() {
+                                            console.log("----------------------------------------------------------");
+                                            console.log("Remaining " + product + ": " + newQuantity + " units");
+                                            console.log("----------------------------------------------------------");
+                                            connection.query(
+                                                "UPDATE bamazon_db.items SET stock_quantity=" +
+                                                //!!!!! use question marks
+                                                newQuantity +
+                                                " WHERE id=" +
+                                                productID,
+                                                function (err, results) {
+                                                    if (err) throw err;
+                                                    console.log('\n');
+                                                    console.log("(Records updated - " + newQuantity + " units)");
+                                                    console.log("----------------------------------------------------------");
+                                                    console.log('\n');
+                                                    console.log("//////////////////////////////////////////////////////////");
+                                                    console.log("----------------------Thank You---------------------------");
+                                                    console.log("-------------------For Your Business----------------------");
+                                                    console.log("//////////////////////////////////////////////////////////");
+                                                    console.log('\n');
+                                                    connection.end();
+                                                }
+                                            );
+                                        }
+
+                                    } else {
+                                        cconsole.log("----------------------------------------------------------");
+                                        console.log("-------------------We have a limited supply----------------");
+                                        console.log("-------------------try a smaller order---------------------");
+                                        console.log("-----------------------------------------------------------");
+                                        connection.end();
+                                    }
+                                } else {
+                                    console.log("----------------------------------------------------------");
+                                    console.log("-------------------Try A different Order------------------");
+                                    console.log("----------------------------------------------------------");
+                                    connection.end();
+                                }
+                            });
+
+                        //* ///////////////////////////////////////////////////////////////////////////
+                        //* Checkout (function)
+                        //* ///////////////////////////////////////////////////////////////////////////
+
+                    }
+                );
+
+                //?
+
+                // connection.end();
+            });
+    });
+}
+
+// function updateInventory(){
+//     connection.query(
+//                 "UPDATE products SET stock_quantity=" + newQuantity + " WHERE id=" + productID, function(err, res) {
+//               console.log(res);
+//             });
+// }
+
 // process.exit();
